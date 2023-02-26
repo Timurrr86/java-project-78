054d07d74aa53591e4e07cb5800644dbd096c300
Timur Valeev <timurrr@gmail.com>
2023-02-26 23:01:20 +0500

    fixed checkstyle error in test

--- app/src/test/java/hexlet/code/NestedSchemaTest.java
+++ app/src/test/java/hexlet/code/NestedSchemaTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class NestedSchemaTest {
 
     @Test
-    public void nestedSchemaTest () {
+    public void nestedSchemaTest() {
         Validator v = new Validator();
 
         MapSchema schema = v.map();
